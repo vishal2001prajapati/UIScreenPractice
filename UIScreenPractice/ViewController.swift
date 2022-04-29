549ed92a47a0b3d4fc36cd68ddadf9217f960bdc
vishal prajapati <vishal.prajapati@simformsolutions.com>
2022-04-29 09:56:37 +0530

    Initial Commit

---
+++ UIScreenPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScreenPractice
+//
+//  Created by Vishal Prajapati on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
